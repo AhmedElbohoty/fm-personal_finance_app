2453ee6d329312f13f755e9e33a3e472d2a2cad8
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-09-12 12:29:00 +0300

    Simple adjustment in AddToPotForm
    - Update style of secondary button

--- src/pages/Pots/AddToPotForm/AddToPotForm.tsx
+++ src/pages/Pots/AddToPotForm/AddToPotForm.tsx
@@ -12,13 +12,13 @@ import ModalInfo from "components/Modal/ModalInfo";
 import ProgressBar from "components/ProgressBar/ProgressBar";
 import { formatPercentage } from "components/ProgressBar/helpers";
 import InputWrapper from "components/Input/InputWrapper";
+import Primary from "components/Buttons/Primary/Primary";
 import InputNumber from "components/Input/InputNumber";
 
 import { PotsPageContext } from "contexts/potsPageContext";
 
 // CSS prefix: .addtopot-
 import "./style.css";
-import Primary from "components/Buttons/Primary/Primary";
 
 function AddToPotForm() {
   const id = useId();
@@ -30,21 +30,24 @@ function AddToPotForm() {
   }
 
   function onChangeAmount(e: ChangeEvent<HTMLInputElement>) {
-    setAmount(Number(e.target.value));
+    let value = Number(e.target.value);
+    const max = addToPot!.target - addToPot!.total;
+    if (value > max) {
+      value = max;
+    }
+    setAmount(value);
   }
 
   function onSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
   }
 
-  console.log(addToPot!.total + amount);
-
   const percentage = ((addToPot!.total + amount) / addToPot!.target) * 100;
   const splitPercentage = (addToPot!.total / addToPot!.target) * 100;
   return (
     <Modal closeModal={closeModal}>
       <ModalHeader
-        text={`Add to ‘${addToPot?.name}’`}
+        text={`Add to ‘${addToPot!.name}’`}
         closeModal={closeModal}
       />
       <ModalInfo text="Add money to your pot to keep it separate from your main balance. As soon as you add this money, it will be deducted from your current balance." />
